b714a3196e9c0daedc0429367f1ac02d183d09a0
iamarjun <arjunmanoj1995@gmail.com>
2020-09-15 00:22:08 +0530

    bound service - local binding

--- app/src/main/java/com/arjun/services/MyService.kt
+++ app/src/main/java/com/arjun/services/MyService.kt
@@ -2,10 +2,30 @@ package com.arjun.services
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
 import timber.log.Timber
+import kotlin.random.Random
 
 class MyService : Service() {
+
+    private var randomNumber: Int = 0
+    private var isRandomGeneratorOn: Boolean = false
+
+    private val MIN = 0
+    private val MAX = 100
+
+    val getRandomNumber
+        get() = randomNumber
+
+    inner class MyServiceBinder : Binder() {
+        val getService: MyService
+            get() = this@MyService
+    }
+
+
+    private val mBinder: IBinder = MyServiceBinder()
+
 
     override fun onCreate() {
         super.onCreate()
@@ -17,16 +37,49 @@ class MyService : Service() {
      */
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Timber.d("onStartCommand")
-        return super.onStartCommand(intent, flags, startId)
+        isRandomGeneratorOn = true
+        Thread {
+            startRandomNumberGenerator()
+        }.start()
+
+        return START_STICKY
     }
 
     override fun onBind(intent: Intent?): IBinder? {
-        return null
+        Timber.d("onBind")
+
+        return mBinder
+    }
+
+    override fun onUnbind(intent: Intent?): Boolean {
+        Timber.d("onUnbind")
+        return super.onUnbind(intent)
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        stopRandomNumberGenerator()
         Timber.d("onDestroy")
+    }
+
+    private fun startRandomNumberGenerator() {
+        while (isRandomGeneratorOn) {
+            try {
+                Thread.sleep(1000)
+                if (isRandomGeneratorOn) {
+                    randomNumber = Random.nextInt(MAX) + MIN
+                    Timber.d("${Thread.currentThread().name} Random Number: $randomNumber")
+                }
+            } catch (e: Exception) {
+                Timber.d("Thread Interrupted")
+            }
+
+        }
+    }
+
+
+    private fun stopRandomNumberGenerator() {
+        isRandomGeneratorOn = false
     }
 
 }

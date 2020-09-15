2dbca15a946e274792a110da1068c2ba4c268cc9
iamarjun <arjunmanoj1995@gmail.com>
2020-09-15 18:26:55 +0530

    implementation for simple intent service

--- app/src/main/java/com/arjun/services/MyService.kt
+++ app/src/main/java/com/arjun/services/MyService.kt
@@ -1,5 +1,6 @@
 package com.arjun.services
 
+import android.app.IntentService
 import android.app.Service
 import android.content.Intent
 import android.os.Binder
@@ -7,24 +8,13 @@ import android.os.IBinder
 import timber.log.Timber
 import kotlin.random.Random
 
-class MyService : Service() {
+class MyService : IntentService(MyService::class.java.canonicalName) {
 
     private var randomNumber: Int = 0
     private var isRandomGeneratorOn: Boolean = false
 
     private val MIN = 0
     private val MAX = 100
-
-    val getRandomNumber
-        get() = randomNumber
-
-    inner class MyServiceBinder : Binder() {
-        val getService: MyService
-            get() = this@MyService
-    }
-
-
-    private val mBinder: IBinder = MyServiceBinder()
 
 
     override fun onCreate() {
@@ -35,25 +25,10 @@ class MyService : Service() {
     /**
      * Gets executed whenever we start a service
      */
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+    override fun onHandleIntent(intent: Intent?) {
         Timber.d("onStartCommand")
         isRandomGeneratorOn = true
-        Thread {
-            startRandomNumberGenerator()
-        }.start()
-
-        return START_STICKY
-    }
-
-    override fun onBind(intent: Intent?): IBinder? {
-        Timber.d("onBind")
-
-        return mBinder
-    }
-
-    override fun onUnbind(intent: Intent?): Boolean {
-        Timber.d("onUnbind")
-        return super.onUnbind(intent)
+        startRandomNumberGenerator()
     }
 
     override fun onDestroy() {
@@ -68,7 +43,7 @@ class MyService : Service() {
                 Thread.sleep(1000)
                 if (isRandomGeneratorOn) {
                     randomNumber = Random.nextInt(MAX) + MIN
-                    Timber.d("${Thread.currentThread().name} Random Number: $randomNumber")
+                    Timber.d("${Thread.currentThread().id} Random Number: $randomNumber")
                 }
             } catch (e: Exception) {
                 Timber.d("Thread Interrupted")
